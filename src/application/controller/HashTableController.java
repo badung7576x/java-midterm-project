917dab6788b0afd93138afe794ebdcb620828962
manh6264@gmail.com <manh6264@gmail.com>
2020-06-02 16:15:26 +0700

    Add queue, about page

---
+++ src/application/controller/HashTableController.java
@@ -0,0 +1,19 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class HashTableController {
+
+	@FXML
+	private GridPane gridpane;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void function() {
+		
+	}
+}
