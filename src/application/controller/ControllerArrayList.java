902b371374dd37d4b0f2e48884dab0d0e9d64559
Đặng Bá Dũng <38398490+badung7576x@users.noreply.github.com>
2020-06-04 22:54:07 +0700

    Revert "Arraylist"

--- src/application/controller/ControllerArrayList.java
+++ src/application/controller/ControllerArrayList.java
@@ -1,12 +1,16 @@
 package application.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ResourceBundle;
+
 import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -16,124 +20,92 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 public class ControllerArrayList implements Initializable {
-
+	
 	@FXML
-	private AnchorPane pane;
+		private AnchorPane pane;
+	@FXML 
+		private TextField valueAdd;
+	@FXML 
+		private TextField indexInsert;
+	@FXML 
+		private TextField valueInsert;
+	@FXML 
+		private TextField indexDelete;
+	@FXML 
+		private Button addItem;
+	@FXML 
+		private Button insertItem;
+	@FXML 
+		private Button deleteItem;
 	@FXML
-	private TextField valueAdd;
-	@FXML
-	private TextField indexInsert;
-	@FXML
-	private TextField valueInsert;
-	@FXML
-	private TextField indexDelete;
-	@FXML
-	private Button addItem;
-	@FXML
-	private Button insertItem;
-	@FXML
-	private Button deleteItem;
-	@FXML
-	private GridPane grid;
-
+		private GridPane grid;
+	  
 	public ArrayList<String> list = new ArrayList<String>();
-
-	public void indexOn(Integer num, String str) {
+	
+	public void indexOn(Integer num,String str) {
 		Button xButton = new Button(str);
 		xButton.setPrefSize(70, 50);
 		xButton.setStyle("-fx-background-color: #abccba;-fx-border-color:gray");
-
+		
 		Label xLabel = new Label(String.valueOf(num));
 		xLabel.setPrefSize(70, 50);
 		xLabel.setMaxWidth(Double.MAX_VALUE);
 		xLabel.setAlignment(Pos.CENTER);
-
+        
 		grid.add(xButton, num, 0);
 		grid.add(xLabel, num, 1);
-
 	}
-
+	
 	public void addItem(ActionEvent event) {
 		String str = valueAdd.getText();
-		if (str.isEmpty())
-			JOptionPane.showMessageDialog(null, "Please input a value !");
-		else {
-			valueAdd.clear();
-			list.add(str);
-			printList();
-		}
+		valueAdd.clear();
+		list.add(str);
+		printList();
 	}
-
-	public static Integer tryParse(String text) {
-		try {
-			return Integer.parseInt(text);
-		} catch (NumberFormatException e) {
-			return null;
-		}
-	}
-
+	
 	public void insertItem(ActionEvent event) {
 		String index = indexInsert.getText();
+		int id = Integer.parseInt(index);
 		String value = valueInsert.getText();
-
-		if (index.isEmpty() || value.isEmpty())
-			JOptionPane.showMessageDialog(null, "Please input a value !");
-		else {
-			if (tryParse(index) == null)
-				JOptionPane.showMessageDialog(null, "Please input a valid index!");
-			else {
-				if (tryParse(index) > list.size())
-					JOptionPane.showMessageDialog(null, "Wrong index!");
-				else {
-					indexInsert.clear();
-					valueInsert.clear();
-					list.add(tryParse(index), value);
-					printList();
-				}
-			}
-		}
+		if(id>list.size())
+			JOptionPane.showMessageDialog(null,"Wrong index!");
+		indexInsert.clear();
+		valueInsert.clear();
+		list.add(id, value);
+		
+		printList();
 	}
-
+	
 	public void deleteItem(ActionEvent event) {
 		String index = indexDelete.getText();
-		if (index.isEmpty())
-			JOptionPane.showMessageDialog(null, "Please input a value !");
-		else {
-			if (tryParse(index) == null)
-				JOptionPane.showMessageDialog(null, "Please input a valid index!");
-			else {
-				System.out.println(list.size());
-				int id = tryParse(index);
-				if (id >= list.size() || id < 0)
-					JOptionPane.showMessageDialog(null, "Wrong index!");
-				else {
-					indexDelete.clear();
-					list.remove(id);
-					grid.getChildren().clear();
-					printList();
-				}
-			}
-		}
-	}
+		int id = Integer.parseInt(index);
+		if(id>list.size()-1)
+			JOptionPane.showMessageDialog(null,"Wrong index!");
+		indexDelete.clear();
+		list.remove(id);
 
-	public void clearAll() {
-		list.clear();
-		grid.getChildren().clear();
+		grid.getChildren().clear();   //clear toan bo gridpane roi render lai
+		printList();
 	}
-
+	
+	public void back() throws IOException {
+		AnchorPane child = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+		pane.getChildren().setAll(child);
+	}
+	
 	public void printList() {
 		Iterator<String> itr = list.iterator();
-		int k = 0;
-		while (itr.hasNext()) {
-			indexOn(k, itr.next());
-			k++;
-		}
+		int k=0;
+	    while (itr.hasNext()) {
+	       indexOn(k, itr.next());
+	       k++;
+	    }
 	}
-
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
